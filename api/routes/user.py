87fe4dfd1c092fda52b42ccc2010c7d838d351d9
Edouard Nicolas <edouard.n@proton.me>
2025-02-01 02:19:41 +0100

    add test and fix response type

--- api/routes/user.py
+++ api/routes/user.py
@@ -18,15 +18,14 @@ router = APIRouter()
 ACCESS_TOKEN_EXPIRE_MINUTES = 60  # Durée d'expiration du token en minutes
 REFRESH_TOKEN_EXPIRE_DAYS = 7  # Durée d'expiration du refresh token en jours
 
-@router.post("/")
-def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
+@router.post("/", status_code=201)
+def create_new_user(user: UserCreate, db: Session = Depends(get_db)) -> UserPrivate:
     """Crée un nouvel utilisateur et retourne un token JWT"""
     user_exist = get_user_by_email(db, user.email)
     if user_exist is not None:
       raise HTTPException(status_code=400, detail="L'utilisateur avec cet email existe déjà dans le système.")
-    create_user(db, user)
-
-    return {"success"}
+    return create_user(db, user)    
+ 
 
 
 @router.post("/token")
